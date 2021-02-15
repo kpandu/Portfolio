cb5b7c4562cb00c0a7b1f123939e67507cff3686
Krishan Panduwawala <kpandu@umd.edu>
2021-02-14 23:39:14 -0500

    added more projects

--- src/components/Projects/NonMobile/projects.jsx
+++ src/components/Projects/NonMobile/projects.jsx
@@ -13,10 +13,20 @@ export default [
     tags: ["Python", "Pygame"],
   },
   {
+    title: "Content Aware Seam Carving",
+    description:
+      "Uses dynamic programming to remove path's of pixels from the bottom of the image to the top with the lowest gradient magnitude. Aka, the pixels with the least amount of importance to the content of the image are carved. This prevents content from being loss from simply cropping an image from its edges.",
+    video: [require("../../../assets/carve_Trim.mp4").default],
+    tags: ["Python"],
+  },
+  {
     title: "Crystal Sky",
     description:
-      "Given a picture of the sky, the program will use CV2 to filter for stars, then using these stars as coordinates and points found through canny edge detection, Delaunay triangulation is applied creating an artistic representation of the image. Edge detection preserves the outline of non-sky elements when triangulated.",
-    imgs: [require("../../../assets/crystal-right.png").default],
+      "Given a picture of the sky, the program will use CV2 to filter for stars, then using these stars as coordinates and points found through canny edge detection, Delaunay triangulation is applied creating an artistic representation of the image. Edge detection preserves the outline of non-sky elements when triangulated. Images in order are after and then before.",
+    imgs: [
+      require("../../../assets/crystal-right.png").default,
+      require("../../../assets/crystal-left.png").default,
+    ],
     tags: ["Python", "CV2"],
   },
   {
@@ -32,6 +42,13 @@ export default [
       "I was blinded by thinking that overly flashy looks good. Before I realized that I was wrong, I made the above graphic which was going to be the at the very top of this website. It is a real-time interactive 3D animation in space made with BabylonJS. Project Midnight is a nice balance between flashy and simple.",
     video: [require("../../../assets/sol3video.mp4").default],
     tags: ["JS", "ReactJS"],
+  },
+  {
+    title: "Undergraduate Research",
+    description:
+      "Engineered a 3 point system detecting drowsy/distracted driving, blinking, and yawning. I worked on creating the CNN for detecting distracted actions, and found optimal performance using YOLO and transfer learning with MobileNetV2. The system was heavily optimized and was run on a Raspberry Pi under the rear-view mirror. Worked with Dr. Teli, Hyekang Joo, and Ashan Panduwawala.",
+    imgs: [require("../../../assets/research.png").default],
+    tags: ["Python", "Tensorflow", "CV2"],
   },
   {
     title: "V1 Parallax",
@@ -63,6 +80,13 @@ export default [
     tags: ["JS", "ReactJS"],
   },
   {
+    title: "Roast Me Alexa Skill",
+    description:
+      "Using the serverless framework, a nodejs AWS lambda function reads in roasts from an external api and alexa reads it out loud.",
+    imgs: [require("../../../assets/roast.jpeg").default],
+    tags: ["JS", "Serverless Framework", "AWS"],
+  },
+  {
     title: "V3 Midnight",
     description:
       "Built a simple dark themed portfolio that preceded the current site. Many things were transfered over to this site.",
@@ -72,5 +96,15 @@ export default [
       require("../../../assets/midnight-3.PNG").default,
     ],
     tags: ["JS", "ReactJS"],
+  },
+  {
+    title: "Pedestrian Detection",
+    description:
+      "Detects pedestrians using HOG descriptors with a perceptron in the first image (less accurate). The second image accomplishes the same thing but this time with a convolutional neural network made with Pytorch.",
+    imgs: [
+      require("../../../assets/hog.png").default,
+      require("../../../assets/pytorch.png").default,
+    ],
+    tags: ["Python", "Pytorch"],
   },
 ];
